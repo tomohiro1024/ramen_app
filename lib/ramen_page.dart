0cc776660773a7940c8cfc84bd2939f552cec36b
tomohiro1024 <gongyezhihong732@gmail.com>
2025-04-02 14:50:39 +0900

    ボタン押下時にアニメーションを追加

--- lib/ramen_page.dart
+++ lib/ramen_page.dart
@@ -9,10 +9,12 @@ class RamenPage extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<RamenPage> {
+  bool _isPressed = false;
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('近くのラーメン屋一覧'),
@@ -33,73 +35,86 @@ class _MyWidgetState extends State<RamenPage> {
                 alignment: Alignment.center,
                 child: Stack(
                   children: [
-                    Container(
-                      height: 140,
-                      width: width * 0.95,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 6,
-                            spreadRadius: 1,
-                            offset: Offset(1, 0.1),
+                    GestureDetector(
+                      onTapDown: (_) => setState(() => _isPressed = true),
+                      onTapUp: (_) => setState(() => _isPressed = false),
+                      onTapCancel: () => setState(() => _isPressed = false),
+                      onTap: () {
+                        // ここにタップ時の処理を追加
+                      },
+                      child: AnimatedScale(
+                        duration: Duration(milliseconds: 100),
+                        scale: _isPressed ? 0.95 : 1.0,
+                        child: Container(
+                          height: 140,
+                          width: width * 0.95,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 6,
+                                spreadRadius: 1,
+                                offset: Offset(1, 0.1),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      child: Row(
-                        children: [
-                          SizedBox(width: width * 0.03),
-                          Image.asset(
-                            'assets/images/sample.png',
-                            width: width * 0.29,
-                            height: 110,
-                            fit: BoxFit.cover,
-                          ),
-                          SizedBox(width: width * 0.03),
-                          SizedBox(
-                            width: width * 0.47,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(height: 25),
-                                Text(
-                                  "埼玉ラーメン埼玉ラーメン埼玉ラーメン埼玉ラーメン",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
-                                ),
-                                SizedBox(height: 10),
-                                Row(
+                          child: Row(
+                            children: [
+                              SizedBox(width: width * 0.03),
+                              Image.asset(
+                                'assets/images/sample.png',
+                                width: width * 0.29,
+                                height: 110,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(width: width * 0.03),
+                              SizedBox(
+                                width: width * 0.47,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Transform.translate(
-                                        offset: Offset(0, 10),
-                                        child: Text("レビュー評価：")),
+                                    SizedBox(height: 25),
                                     Text(
-                                      "4.0",
+                                      "埼玉ラーメン埼玉ラーメン埼玉ラーメン埼玉ラーメン",
                                       style: TextStyle(
-                                        fontSize: 45,
+                                        fontSize: 18,
                                         color: Colors.black,
                                         fontWeight: FontWeight.bold,
                                       ),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                    ),
+                                    SizedBox(height: 10),
+                                    Row(
+                                      children: [
+                                        Transform.translate(
+                                            offset: Offset(0, 10),
+                                            child: Text("レビュー評価：")),
+                                        Text(
+                                          "4.0",
+                                          style: TextStyle(
+                                            fontSize: 45,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ],
                                 ),
-                              ],
-                            ),
+                              ),
+                              Icon(
+                                Icons.chevron_right,
+                                size: 50,
+                              ),
+                            ],
                           ),
-                          Icon(
-                            Icons.chevron_right,
-                            size: 50,
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                    )
+
                     // Positioned(
                     //   top: 0,
                     //   right: 10,
--- lib/sort.dart
+++ lib/sort.dart
@@ -9,35 +9,48 @@ class Sort extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<Sort> {
+  bool _isPressed = false;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: widget.width * 0.02),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 6,
-              spreadRadius: 1,
-              offset: Offset(1, 1),
+    return GestureDetector(
+      onTapDown: (_) => setState(() => _isPressed = true),
+      onTapUp: (_) => setState(() => _isPressed = false),
+      onTapCancel: () => setState(() => _isPressed = false),
+      onTap: () {
+        // ここにタップ時の処理を追加
+      },
+      child: AnimatedScale(
+        duration: Duration(milliseconds: 100),
+        scale: _isPressed ? 0.95 : 1.0,
+        child: Padding(
+          padding: EdgeInsets.only(left: widget.width * 0.02),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  spreadRadius: 1,
+                  offset: Offset(1, 1),
+                ),
+              ],
             ),
-          ],
-        ),
-        height: 35,
-        width: widget.width * 0.35,
-        child: Row(
-          children: [
-            SizedBox(width: widget.width * 0.02),
-            Icon(Icons.sort),
-            SizedBox(width: widget.width * 0.05),
-            Text(
-              '近い順',
-              style: TextStyle(fontSize: 15, color: Colors.black),
+            height: 35,
+            width: widget.width * 0.35,
+            child: Row(
+              children: [
+                SizedBox(width: widget.width * 0.02),
+                Icon(Icons.sort),
+                SizedBox(width: widget.width * 0.05),
+                Text(
+                  '近い順',
+                  style: TextStyle(fontSize: 15, color: Colors.black),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
