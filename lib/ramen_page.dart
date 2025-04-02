a7b69b5847ff7588f4d9aa2ef6f4a9bd953cde6e
tomohiro1024 <gongyezhihong732@gmail.com>
2025-04-02 14:37:32 +0900

    微修正

--- lib/ramen_page.dart
+++ lib/ramen_page.dart
@@ -31,84 +31,86 @@ class _MyWidgetState extends State<RamenPage> {
               SizedBox(height: 15),
               Align(
                 alignment: Alignment.center,
-                child: Stack(children: [
-                  Container(
-                    height: 140,
-                    width: width * 0.95,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black26,
-                          blurRadius: 6,
-                          spreadRadius: 1,
-                          offset: Offset(1, 0.1),
-                        ),
-                      ],
-                    ),
-                    child: Row(
-                      children: [
-                        SizedBox(width: width * 0.03),
-                        Image.asset(
-                          'assets/images/sample.png',
-                          width: width * 0.29,
-                          height: 110,
-                          fit: BoxFit.cover,
-                        ),
-                        SizedBox(width: width * 0.03),
-                        SizedBox(
-                          width: width * 0.47,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(height: 25),
-                              Text(
-                                "埼玉ラーメン埼玉ラーメン埼玉ラーメン埼玉ラーメン",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 1,
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                children: [
-                                  Transform.translate(
-                                      offset: Offset(0, 10),
-                                      child: Text("レビュー評価：")),
-                                  Text(
-                                    "4.0",
-                                    style: TextStyle(
-                                      fontSize: 45,
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: width * 0.95,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 6,
+                            spreadRadius: 1,
+                            offset: Offset(1, 0.1),
                           ),
-                        ),
-                        Icon(
-                          Icons.chevron_right,
-                          size: 50,
-                        ),
-                      ],
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          SizedBox(width: width * 0.03),
+                          Image.asset(
+                            'assets/images/sample.png',
+                            width: width * 0.29,
+                            height: 110,
+                            fit: BoxFit.cover,
+                          ),
+                          SizedBox(width: width * 0.03),
+                          SizedBox(
+                            width: width * 0.47,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 25),
+                                Text(
+                                  "埼玉ラーメン埼玉ラーメン埼玉ラーメン埼玉ラーメン",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  children: [
+                                    Transform.translate(
+                                        offset: Offset(0, 10),
+                                        child: Text("レビュー評価：")),
+                                    Text(
+                                      "4.0",
+                                      style: TextStyle(
+                                        fontSize: 45,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          Icon(
+                            Icons.chevron_right,
+                            size: 50,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  // Positioned(
-                  //   top: 0,
-                  //   right: 10,
-                  //   child: Icon(
-                  //     Icons.star,
-                  //     size: 30,
-                  //     color: Colors.black,
-                  //   ),
-                  // ),
-                ]),
+                    // Positioned(
+                    //   top: 0,
+                    //   right: 10,
+                    //   child: Icon(
+                    //     Icons.star,
+                    //     size: 30,
+                    //     color: Colors.black,
+                    //   ),
+                    // ),
+                  ],
+                ),
               ),
             ],
           ),
