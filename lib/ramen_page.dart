781d81c99550afbb3ee48047cbdf8cd0912b7bff
tomohiro1024 <gongyezhihong732@gmail.com>
2025-04-02 10:01:32 +0900

    ソートUIの追加

--- lib/ramen_page.dart
+++ lib/ramen_page.dart
@@ -10,10 +10,55 @@ class RamenPage extends StatefulWidget {
 class _MyWidgetState extends State<RamenPage> {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('近くのラーメン屋一覧'),
         backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        color: Color(0xFFF5E1A4),
+        height: height,
+        width: width,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Padding(
+                padding: EdgeInsets.only(left: width * 0.02),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 6, // ぼかしの強さ
+                        spreadRadius: 1, // 影の広がり具合
+                        offset: Offset(1, 1), // 影の位置（X, Y）
+                      ),
+                    ],
+                  ),
+                  height: 35,
+                  width: width * 0.35,
+                  child: Row(
+                    children: [
+                      SizedBox(width: width * 0.02),
+                      Icon(Icons.sort),
+                      SizedBox(width: width * 0.05),
+                      Text(
+                        '近い順',
+                        style: TextStyle(fontSize: 15, color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
